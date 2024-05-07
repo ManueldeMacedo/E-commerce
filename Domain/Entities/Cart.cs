641d6db81cc8e9f9518b08a145bf26dcebc5e167
Fede Alvarado <federico.alvarado96@gmail.com>
2024-05-07 20:46:15 -0300

    New Class

---
+++ Domain/Entities/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Enum;
+
+namespace Domain.Entities
+{
+    public class Cart
+    {
+        public int Order { get; set; }
+        public int AmountProduct { get; set; }
+        public ICollection<Product> ListProducts { get; set; }
+        public double TotalPrice { get; set; }
+        public TypePayment typePayment { get; set; }
+
+    }
+
+}
