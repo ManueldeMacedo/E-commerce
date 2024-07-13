bfefa817fc81370b939cfa73e311e9339d14cb6a
francocallegari <francocallegari12@gmail.com>
2024-07-13 17:03:27 -0300

    User entitie modified

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,6 +1,8 @@
 ﻿using Domain.Enum;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +11,37 @@ namespace Domain.Entities
 {
     public class User
     {
-        public int Id { get ; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(64)")]
         public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(128)")]
         public string Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(128)")]
         public string Password { get; set; }
-        // Ver Required
+
+        [Required]
+        [Column(TypeName = "nvarchar(128)")]
         public string UserName { get; set; }
+
         // Diferencia el tipo de usuario, admin o comun
+        [Required]
         public UserType UserType { get; set; }
+
         // Atributos para la fecha de alta y baja de usuarios
+        [Required]
+        [Column(TypeName = "datetime")]
         public DateTime UserRegistrationDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "datetime")]
         public DateTime UserDeletionDate { get; set; }
 
         public ICollection<Cart>Carts { get; set; }
---
+++ Infrastructure/Migrations/20240713195859_userChanges.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class userChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UserRegistrationDate",
+                table: "Users",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UserDeletionDate",
+                table: "Users",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(64)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UserRegistrationDate",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UserDeletionDate",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+        }
+    }
+}
